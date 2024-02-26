d9a4034e787e6cef91d780387324984d00ea648e
ahmathlete <Ahmed.a.b.Homoudi@gmx.de>
2024-02-26 22:02:43 +0100

    Swirl

---
+++ AquaFortR_Swirl/First_Lesson/add_f.f90
@@ -0,0 +1,6 @@
+subroutine add_f(x, y, answer)
+  ! add 2 numbers
+  implicit none
+  double precision :: x, y, answer
+  answer = x + y
+end
---
+++ AquaFortR_Swirl/add_f.f90
@@ -0,0 +1,6 @@
+subroutine add_f(x, y, answer)\n
+  ! add 2 numbers\n
+  implicit none\n
+  double precision :: x, y, answer
+  answer = x + y
+end
